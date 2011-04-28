1bf9ee500649e8063d695a20a3aa6c1edbb6a7b9
Matthew Butler <matthewtbutler@gmail.com>
2011-04-27 19:51:23 -0500

    http://www.triconsole.com/php/calendar_datepicker.php

---
+++ datepicker.php
@@ -0,0 +1,28 @@
+<?php 
+
+	require_once('calendar/classes/tc_calendar.php');
+	$date3_default = "2011-03-10";
+	$date4_default = "2011-03-30";
+	
+	$myCalendar = new tc_calendar("date3", true, false);
+	$myCalendar->setIcon("calendar/images/iconCalendar.gif");
+	$myCalendar->setDate(date('d', strtotime($date3_default))
+		, date('m', strtotime($date3_default))
+		, date('Y', strtotime($date3_default)));
+	$myCalendar->setPath("calendar/");
+	$myCalendar->setYearInterval(1970, 2020);
+	$myCalendar->setDatePair('date3', 'date4', $date4_default);
+	$myCalendar->writeScript();	  
+	
+	$myCalendar = new tc_calendar("date4", true, false);
+	$myCalendar->setIcon("calendar/images/iconCalendar.gif");
+	$myCalendar->setDate(date('d', strtotime($date4_default))
+	   , date('m', strtotime($date4_default))
+	   , date('Y', strtotime($date4_default)));
+	$myCalendar->setPath("calendar/");
+	$myCalendar->setYearInterval(1970, 2020);
+	$myCalendar->setAlignment('left', 'bottom');
+	$myCalendar->setDatePair('date3', 'date4', $date3_default);
+	$myCalendar->writeScript();	  
+
+?>
