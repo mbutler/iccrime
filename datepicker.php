c69388a561e54f0e5e329ef3f3256126ec19b1d2
Matthew Butler <matthewtbutler@gmail.com>
2011-04-28 19:48:02 -0500

    added conditionals

--- datepicker.php
+++ datepicker.php
@@ -1,9 +1,25 @@
 <?php 
 
 	require_once('calendar/classes/tc_calendar.php');
-	$date3_default = "2011-03-10";
-	$date4_default = "2011-03-30";
+	$todayDate = date("Ymd"); 
 	
+	//sets the default dates to current date unless they've been posted
+	if (isset($_POST['date3']))
+	{
+		$date3_default = $_POST['date3'];
+	} else
+	{
+		$date3_default = $todayDate;
+	}
+	
+	if (isset($_POST['date4']))
+	{
+		$date4_default = $_POST['date4'];
+	} else
+	{
+		$date4_default = $todayDate;
+	}	
+		
 	$myCalendar = new tc_calendar("date3", true, false);
 	$myCalendar->setIcon("calendar/images/iconCalendar.gif");
 	$myCalendar->setDate(date('d', strtotime($date3_default))
