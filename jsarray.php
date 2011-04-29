b5d6377b5fea7ff7fba731b88ee944c694c815e6
Matthew Butler <matthewtbutler@gmail.com>
2011-04-28 22:38:23 -0500

    fixed map not showing if no results.  had to unset array if zero rows returned

--- jsarray.php
+++ jsarray.php
@@ -38,8 +38,12 @@ if ($charge == "All Charges")
 
 mysql_close('$con');
 
+// get the number of rows in the result
+$numRows = mysql_num_rows($result);
+
 $totalCharges = 0;
 
+// start constructing the array
 $places = "var places = new Array(";
 
 while ($rows = mysql_fetch_array($result))
@@ -60,13 +64,17 @@ while ($rows = mysql_fetch_array($result))
 	
 }
 
-
 $places = substr($places, 0, -1);
 $places .= ");\n";
 
-echo $places;
+//if there are no results then remove the array
+if($numRows == 0) 
+{
+	unset($places);
+} else
+{
+	echo $places;
+}
 
-
-//echo "Total charges: $totalCharges";
 
 ?>
