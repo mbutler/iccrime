dc4ccb5af579ba5b48a22067c564ac686b3c9278
Matthew Butler <matthewtbutler@gmail.com>
2011-04-30 14:41:47 -0500

    add options for individual cities, dorms, and all locations.  Put condition for homeDesc in city-select instead of jsarray.php

---
+++ city-select.php
@@ -0,0 +1,41 @@
+<?php
+
+if(isset($_POST['home'])) 
+{
+	$homeDesc = $_POST['home'];
+	
+	if ($homeDesc == "")
+	{
+		$homeDesc = "All Locations";
+	}
+} else
+{
+	$homeDesc = "--Home Location--";
+}
+
+?>
+
+<select name="home">
+<option selected  value="<?php echo $homeDesc; ?>"><?php echo $homeDesc; ?></option>
+<optgroup label="Default">
+	<option value="">All Locations</option>
+</optgroup>
+
+<optgroup label="Cities">
+    <option value="Cedar Rapids">Cedar Rapids</option>
+    <option value="Coralville">Coralville</option>
+    <option value="Chicago">Chicago</option>
+    <option value="North Liberty">North Liberty</option>
+</optgroup>
+    <optgroup label="Dorms">
+    <option value="Burge">Burge</option>
+    <option value="Currier">Currier</option>
+    <option value="Daum">Daum</option>
+    <option value="Hillcrest">Hillcrest</option>
+    <option value="Mayflower">Mayflower</option>
+    <option value="Quadrangle">Quadrangle</option>
+    <option value="Rienow">Rienow</option>
+    <option value="Slater">Slater</option>
+    <option value="Stanley">Stanley</option>
+</optgroup>
+</select>
