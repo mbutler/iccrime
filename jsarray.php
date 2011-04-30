89dd4a68a85714fb006de4f0c3076ca01a09afc3
Matthew Butler <matthewtbutler@gmail.com>
2011-04-30 16:23:48 -0500

    took out extraneous comments

--- jsarray.php
+++ jsarray.php
@@ -1,6 +1,5 @@
 <?php
 
-// Testing Git
 // CREATES THE JAVASCRIPT ARRAY FROM A DATABASE QUERY
 require_once('connect.php');
 
@@ -9,24 +8,8 @@ $home = isset($_POST['home']) ? $_POST['home'] : "";
 $beginDate = isset($_REQUEST["date3"]) ? $_REQUEST["date3"] : "";
 $endDate = isset($_REQUEST["date4"]) ? $_REQUEST["date4"] : "";
 
+if ($home == "Unknown Address") $home = "unknown";
 
-//basic
-//$result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE charges.charge = ' $charge ' GROUP BY arrests.name") or die(mysql_error());
-
-//Sorts by charges and address
-//$result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE charges.charge = ' $charge ' AND arrests.address LIKE '%chicago%' GROUP BY arrests.name") or die(mysql_error());
-
-//Sorts on keywords, good for presets
-//$result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE charges.charge LIKE '%Marijuana%' GROUP BY arrests.name") or die(mysql_error());
-
-//Everyone from Chicago
-//$result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE address LIKE '%chicago%'");
-
-//All arrests from Chicago and Cedar Rapids people
-//$result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE arrests_id >571 AND arrests.address LIKE '%chicago%' OR arrests.address LIKE '%cedar rapids%' GROUP BY arrests.name");
-
-//Everything in the last week using variables
-//$result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE charges.charge = ' $charge ' AND DATE_SUB(CURDATE(),INTERVAL $intervalLength $intervalType) <= arrest_date GROUP BY arrests.name") or die(mysql_error());
 
 //charges in a date range
 $result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE charges.charge = ' $charge ' AND arrest_date BETWEEN '$beginDate' AND '$endDate' AND arrests.address LIKE '%$home%' GROUP BY arrests.name") or die(mysql_error());
