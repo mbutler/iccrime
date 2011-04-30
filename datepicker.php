0738374f2db8e8a207fdb196e233d735c1a9a6a2
Matthew Butler <matthewtbutler@gmail.com>
2011-04-30 14:01:17 -0500

    changed date format

--- datepicker.php
+++ datepicker.php
@@ -19,26 +19,33 @@
 	{
 		$date4_default = $todayDate;
 	}	
-		
+	
 	$myCalendar = new tc_calendar("date3", true, false);
 	$myCalendar->setIcon("calendar/images/iconCalendar.gif");
-	$myCalendar->setDate(date('d', strtotime($date3_default))
-		, date('m', strtotime($date3_default))
-		, date('Y', strtotime($date3_default)));
+	$myCalendar->setDate(1, 9, 2010);
+	$myCalendar->setDateFormat('m.d.y');
+	//$myCalendar->setDateFormat('F j, Y');
 	$myCalendar->setPath("calendar/");
 	$myCalendar->setYearInterval(1970, 2020);
 	$myCalendar->setDatePair('date3', 'date4', $date4_default);
-	$myCalendar->writeScript();	  
+	$myCalendar->writeScript();	 
+	echo "<br /><br />";
+	
+	
 	
 	$myCalendar = new tc_calendar("date4", true, false);
 	$myCalendar->setIcon("calendar/images/iconCalendar.gif");
 	$myCalendar->setDate(date('d', strtotime($date4_default))
 	   , date('m', strtotime($date4_default))
 	   , date('Y', strtotime($date4_default)));
+	$myCalendar->setDateFormat('m.d.y');
+	//$myCalendar->setDateFormat('F j, Y');
 	$myCalendar->setPath("calendar/");
 	$myCalendar->setYearInterval(1970, 2020);
 	$myCalendar->setAlignment('right', 'top');
 	$myCalendar->setDatePair('date3', 'date4', $date3_default);
-	$myCalendar->writeScript();	  
+	$myCalendar->writeScript();	
+	
+	
 
 ?>
