a27cbe70974db4080058706ac3080fd9ab8f5ad7
Matthew Butler <matthewtbutler@gmail.com>
2011-05-02 20:00:41 -0500

    added age query and changed REQUESTs to POSTs

--- jsarray.php
+++ jsarray.php
@@ -5,18 +5,20 @@ require_once('connect.php');
 
 $charge = isset($_POST['charge']) ? $_POST['charge'] : "";
 $home = isset($_POST['home']) ? $_POST['home'] : "";
-$beginDate = isset($_REQUEST["date3"]) ? $_REQUEST["date3"] : "";
-$endDate = isset($_REQUEST["date4"]) ? $_REQUEST["date4"] : "";
+$beginDate = isset($_POST["date3"]) ? $_POST["date3"] : "";
+$endDate = isset($_POST["date4"]) ? $_POST["date4"] : "";
+$startAge = isset($_POST["start-age"]) ? $_POST["start-age"] : "";
+$endAge = isset($_POST["end-age"]) ? $_POST["end-age"] : "";
 
 if ($home == "Unknown Address") $home = "unknown";
 
 
 //charges in a date range
-$result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE charges.charge = ' $charge ' AND arrest_date BETWEEN '$beginDate' AND '$endDate' AND arrests.address LIKE '%$home%' GROUP BY arrests.name") or die(mysql_error());
+$result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE charges.charge = ' $charge ' AND arrest_date BETWEEN '$beginDate' AND '$endDate' AND arrests.address LIKE '%$home%' AND age >= '$startAge' and age <= '$endAge' GROUP BY arrests.name") or die(mysql_error());
 
 if ($charge == "All Charges")
 {
-	$result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE arrests_id >571 AND arrest_date BETWEEN '$beginDate' AND '$endDate' AND arrests.address LIKE '%$home%' GROUP BY arrests.name");
+	$result = mysql_query("SELECT * FROM arrests JOIN charges ON arrests.incident = charges.incident WHERE arrests_id >571 AND arrest_date BETWEEN '$beginDate' AND '$endDate' AND arrests.address LIKE '%$home%' AND age >= '$startAge' and age <= '$endAge' GROUP BY arrests.name");
 }
 
 mysql_close('$con');
